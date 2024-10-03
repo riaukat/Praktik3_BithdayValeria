2906c950129acad123a08d7df3df861f3fd0aeec
Pr1 <valeriauka04@gmail.com>
2024-10-03 21:48:24 +0700

    Tambah Gambar

--- app/src/main/java/com/valeriaukat/happybirthday/MainActivity.kt
+++ app/src/main/java/com/valeriaukat/happybirthday/MainActivity.kt
@@ -20,7 +20,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.valeriaukat.happybirthday.ui.theme.HappyBirthdayTheme
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 
 // Kelas utama yang menjalankan aplikasi
